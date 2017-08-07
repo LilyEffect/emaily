fcee884192e88f8b6f2d83a3734e2cfeddeb1a0c
Jay Lee <junghwan@princeton.edu>
2017-08-07 19:12:25 +0000

    Added OAuth Authentication flow

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
+
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({ hello: 'world' });
-})
+passport.use(new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: `/auth/google/callback`
+}, (accessToken) => {
+    console.log(accessToken);
+}));
 
-const port = process.env.PORT || 3000;
+app.get('/auth/google', passport.authenticate('google', {
+    scope: ['profile', 'email']
+}));
+
+const port = process.env.PORT || 5000;
 app.listen(port);
 console.log(`App started on port ${port}`);
